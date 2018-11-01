52afc012c86868b1b3e2a4e1ec9f625ed5f2e43a
Clint Milner <clinton.milner@rebasoft.net>
2018-11-01 09:04:29 +0000

    working on layouts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,120 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Dropbox} from 'dropbox';
+import {
+    Container,
+    Row,
+    Col,
+    NavbarBrand,
+    Navbar,
+    ListGroupItem,
+    ListGroup,
+    Table,
+    Card,
+    CardImg, CardBody, CardTitle, CardHeader, CardImgOverlay, CardText
+} from 'reactstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Container fluid>
+                    <Navbar color="dark" expand="md">
+                        <NavbarBrand href="/">dTunes</NavbarBrand>
+                    </Navbar>
+                <Row>
+                    <Col xs={3}>
+                        <ListGroup>
+                            <ListGroupItem>Cras justo odio</ListGroupItem>
+                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                            <ListGroupItem>Morbi leo risus</ListGroupItem>
+                            <ListGroupItem>Morbi leo risus</ListGroupItem>
+                            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                            <ListGroupItem>Cras justo odio</ListGroupItem>
+                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                            <ListGroupItem>Morbi leo risus</ListGroupItem>
+                            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                            <ListGroupItem>Cras justo odio</ListGroupItem>
+                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                            <ListGroupItem>Morbi leo risus</ListGroupItem>
+                            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                            <ListGroupItem>Cras justo odio</ListGroupItem>
+                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                            <ListGroupItem>Morbi leo risus</ListGroupItem>
+                            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                            <ListGroupItem>Cras justo odio</ListGroupItem>
+                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                            <ListGroupItem>Morbi leo risus</ListGroupItem>
+                            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                            <ListGroupItem>Cras justo odio</ListGroupItem>
+                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                            <ListGroupItem>Morbi leo risus</ListGroupItem>
+                            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                        </ListGroup>
+                    </Col>
+                    <Col xs={9}>
+                        <Card>
+                            <CardHeader tag="h3">
+                                <CardTitle>Card title</CardTitle>
+                            </CardHeader>
+
+                            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                            {/*<CardBody>*/}
+                                <Table dark responsive>
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Table heading</th>
+                                        <th>Table heading</th>
+                                        <th>Table heading</th>
+                                        <th>Table heading</th>
+                                        <th>Table heading</th>
+                                        <th>Table heading</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                        <td>Table cell</td>
+                                    </tr>
+                                    </tbody>
+                                </Table>
+                            {/*</CardBody>*/}
+                        </Card>
+
+                    </Col>
+                </Row>
+
+            </Container>
+        );
+    }
 }
 
 export default App;
